7b551b1f25c2126475e82422c1873127257e9318
sindi16 <10029@holbertonstudents.com>
2025-04-25 16:08:43 +0200

    adding env file to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { VehiclesModule } from './vehicles/vehicles.module';
@@ -9,20 +10,28 @@ import { VehicleEntity } from './vehicles/entity/vehicle.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: 'root',
-      database: 'luxury-rides',
-      entities: [UserEntity,VehicleEntity],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        })
+      ],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_DATABASE"),
+        entities: [UserEntity, VehicleEntity],
+        synchronize: true,
+      })
     })
-    ,UsersModule,
+    , UsersModule,
     AuthModule,
     VehiclesModule,
     BookingModule
   ]
 })
-export class AppModule {}
+export class AppModule { }
